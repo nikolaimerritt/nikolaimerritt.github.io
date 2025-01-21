7fe577f9a2833a9bfc1c4d53f5a5d97b63f3262f
Nikolai Merritt <nikolaimerritt20@gmail.com>
2025-01-21 21:29:53 +0000

    moved everything to src/

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  server: {
+    open: 'index.html',
+  },
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '.',
+  },
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
